a9099b2eb4891963301089301f1da4b921b61cfe
LOUCIF-Raouf <raoufloucif96@gmail.com>
2019-11-26 13:05:08 +0100

    snake_case

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -1,6 +1,5 @@
-
 function ucfirst(str) {
-    if(typeof  str !== "string" || str.length===0) return "";
+    if (typeof str !== "string" || str.length === 0) return "";
     return str[0].toUpperCase() + str.substring(1);
 }
 
@@ -14,17 +13,27 @@ console.log(ucfirst({}));
 
 function capitalize(str) {
 
-    return   str.toLowerCase()
-                .split(' ')
-                .map(function (item) {
-                    return ucfirst(item);
-                })
-                .join(" ");
+    return str.toLowerCase()
+        .split(' ')
+        .map(function (item) {
+            return ucfirst(item);
+        })
+        .join(" ");
 }
 
 console.log(capitalize('raouf loucif'));
 
 function camelCse(str) {
-    return capitalize(str).replace(' ','');
+    return capitalize(str).replace(' ', '');
 }
+
 console.log(camelCse('raouf loucif'));
+
+function snake_case(str) {
+         return str.toLowerCase().split(' ')
+            .map(function (item) {
+                return item.replace(',','');
+            })
+            .join("-");
+}
+console.log(snake_case("Hello World, i am john"));
