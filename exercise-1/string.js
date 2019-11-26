cfa60445791a009c945346ebbaa753b0f44a5ce7
LOUCIF-Raouf <raoufloucif96@gmail.com>
2019-11-26 12:36:54 +0100

    methods js

---
+++ exercise-1/string.js
@@ -0,0 +1,30 @@
+
+function ucfirst(str) {
+    if(typeof  str !== "string" || str.length===0) return "";
+    return str[0].toUpperCase() + str.substring(1);
+}
+
+console.log(ucfirst('test'));
+console.log(ucfirst('Test'));
+console.log(ucfirst('rest raouf'));
+console.log(ucfirst('3est'));
+console.log(ucfirst(''));
+console.log(ucfirst(null));
+console.log(ucfirst({}));
+
+function capitalize(str) {
+
+    return   str.toLowerCase()
+                .split(' ')
+                .map(function (item) {
+                    return ucfirst(item);
+                })
+                .join(" ");
+}
+
+console.log(capitalize('raouf loucif'));
+
+function camelCse(str) {
+    return capitalize(str).replace(' ','');
+}
+console.log(camelCse('raouf loucif'));
--- intro/hello.js
+++ intro/hello.js
@@ -1,1 +1,4 @@
+
 console.log("Hello World !");
+
+let x ="raouf";
