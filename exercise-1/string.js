ae0f97a4745a67dd2fab1ee75714b6637ceb119b
LOUCIF-Raouf <raoufloucif96@gmail.com>
2019-11-27 10:40:15 +0100

    leet

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -30,10 +30,52 @@ function camelCse(str) {
 console.log(camelCse('raouf loucif'));
 
 function snake_case(str) {
-         return str.toLowerCase().split(' ')
-            .map(function (item) {
-                return item.replace(',','');
-            })
-            .join("-");
+    return str.toLowerCase().split(' ')
+        .map(function (item) {
+            return item.replace(',', '');
+        })
+        .join("-");
 }
+
 console.log(snake_case("Hello World, i am john"));
+
+function leet(str) {
+    let char = '';
+    let newstr = '';
+    for (let i = 0; i < str.length; i++) {
+        char = str.charAt(i);
+        switch (char) {
+            case'a':
+                newstr += 4;
+                break;
+            case'e':
+                newstr += 3;
+                break;
+            case'i':
+                newstr += 1;
+                break;
+            case'o':
+                newstr += 0;
+                break;
+            case'u':
+                newstr += "(_)";
+                break;
+            case ('y'):
+                newstr += 7;
+                break;
+
+            default:
+                newstr += char;
+        }
+    }
+
+    return newstr;
+
+}
+
+console.log(leet("anaconda"));
+
+
+function verlan() {
+    
+}
