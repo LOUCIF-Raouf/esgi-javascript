02c3945667a828f96f316491b92da0f28528c521
LOUCIF-Raouf <raoufloucif96@gmail.com>
2019-11-29 14:23:04 +0100

    type_check_v1 1.2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -2,9 +2,9 @@ function type_check_v1(val, type) {
     let typeVal = typeof val;
 
     if (typeVal !== 'object' && typeVal === type) return true;
-    if (typeVal === "object" && type == "null") return true;
-    if (typeVal === "object" && type == "array") return true;
-    if (typeVal === "object" && type == "object") return true;
+    if (typeVal === "object" && ( type == "null" || type == "array" || type == "object" )) return true;
+    // if (typeVal === "object" && ) return true;
+
     else return false ;
 
 
@@ -21,4 +21,7 @@ console.log("null_true: ", type_check_v1(null, "null"));
 console.log("null_false: ", type_check_v1(null, "object"));
 console.log("array_true: ", type_check_v1([0,2], "array"));
 console.log("array_false: ", type_check_v1([0,2], "object"));
+console.log("undefined_true ", type_check_v1(undefined, "undefined"));
+console.log("undefined_false ", type_check_v1(undefined, "object"));
+
 
