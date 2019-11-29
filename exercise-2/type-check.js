b78326a795808c589722226f94e06d7ff08063c7
LOUCIF-Raouf <raoufloucif96@gmail.com>
2019-11-29 15:03:19 +0100

    type_check_v1 1.4

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,27 +1,26 @@
 function type_check_v1(val, type) {
     let typeVal = typeof val;
+    console.log(typeVal);
+    if(typeVal === type && typeVal === 'object') return false;
+    if (typeVal === type && typeVal !== 'undefined') return true;
+    if (typeVal === "object" && type!=="object" && (type === "null" || type === "array")) return true;
 
-    if (typeVal !== 'object' && typeVal === type) return true;
-    if (typeVal === "object" && ( type == "null" || type == "array" || type == "object" )) return true;
-    // if (typeVal === "object" && ) return true;
-
-    else return false ;
+    else return false;
 
 
 }
 
-//les null et array return null et array
-console.log("number_true: ", type_check_v1(1, "number"));
-console.log("number_false: ", type_check_v1(1, "string"));
-console.log("string_true: ", type_check_v1("raouf", "string"));
-console.log("string_false: ", type_check_v1("raouf", "number"));
-console.log("bool_true: ", type_check_v1(true, "boolean"));
-console.log("bool_false: ", type_check_v1(false, "string"));
-console.log("null_true: ", type_check_v1(null, "null"));
-console.log("null_false: ", type_check_v1(null, "object"));
-console.log("array_true: ", type_check_v1([0,2], "array"));
-console.log("array_false: ", type_check_v1([0,2], "object"));
-console.log("undefined_true ", type_check_v1(undefined, "undefined"));
-console.log("undefined_false ", type_check_v1(undefined, "object"));
+// //les null et array return null et array
+// console.log("number_true: ", type_check_v1(1, "number"));
+// console.log("number_false: ", type_check_v1(1, "string"));
+// console.log("string_true: ", type_check_v1("raouf", "string"));
+// console.log("string_false: ", type_check_v1("raouf", "number"));
+// console.log("bool_true: ", type_check_v1(true, "boolean"));
+// console.log("bool_false: ", type_check_v1(false, "string"));
+// console.log("null_true: ", type_check_v1(null, "null"));
+// console.log("null_false: ", type_check_v1(null, "object"));
+// console.log("array_true: ", type_check_v1([0,2], "array"));
+// console.log("array_false: ", type_check_v1([0,2], "object"));
+ console.log("nullNotObject_false ", type_check_v1(null, "object"));
 
 
